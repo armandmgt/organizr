68361ad789cbfe5bd87c2f02c4afed439d58e9a6
Armand Mégrot <armand.megrot@cashbee.fr>
2019-11-07 02:36:21 +0000

    Update env vars

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { connect, connection, model, Schema, set } from 'mongoose';
 set('useCreateIndex', true);
 
 connect(
-  'mongodb://armandmgt.fr:27017/organizr',
+  `mongodb://root:${process.env.MONGO_INITDB_ROOT_PASSWORD}@armandmgt.fr:27017/organizr`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
