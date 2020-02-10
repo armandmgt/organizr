af3b383aa38f3d430350b01ca1652def94a33f3d
Armand Mégrot <armand.megrot@cashbee.fr>
2020-02-10 02:22:43 +0000

    Update deploy script to fix directory structure

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 BUNDLE_FILENAME="bundle-$(git rev-parse --short HEAD).zip"
-cd ..
-zip -r terraform/${BUNDLE_FILENAME} package.json yarn.lock .npmrc build/*
-cd -
+pushd ..
+zip -r terraform/${BUNDLE_FILENAME} package.json yarn.lock .npmrc
+pushd build
+zip -ur ../terraform/${BUNDLE_FILENAME} *
+popd
+popd
 terraform apply --var-file=vars.tfvars --var "dist=$BUNDLE_FILENAME"
 aws --profile armandmgt elasticbeanstalk update-environment --environment-name $(terraform output env_name) --version-label $(terraform output app_version) > /dev/null
 rm ${BUNDLE_FILENAME}
