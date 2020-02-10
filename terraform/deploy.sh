a2bfb9320a872aebb8fd4b929db978117d48487a
Armand Mégrot <armand.megrot@cashbee.fr>
2020-02-10 02:02:04 +0000

    Update deploy script

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -2,8 +2,8 @@
 
 BUNDLE_FILENAME="bundle-$(git rev-parse --short HEAD).zip"
 cd ..
-zip -r terraform/${BUNDLE_FILENAME} package.json build/*
+zip -r terraform/${BUNDLE_FILENAME} package.json yarn.lock .npmrc build/*
 cd -
 terraform apply --var-file=vars.tfvars --var "dist=$BUNDLE_FILENAME"
-aws --profile armandmgt elasticbeanstalk update-environment --environment-name $(terraform output env_name) --version-label $(terraform output app_version)
+aws --profile armandmgt elasticbeanstalk update-environment --environment-name $(terraform output env_name) --version-label $(terraform output app_version) > /dev/null
 rm ${BUNDLE_FILENAME}
